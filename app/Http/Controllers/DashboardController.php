e0d4a6934a9be183bdc5341e754856a56461cab7
Renan Gabriel <renan.testc@gmail.com>
2023-10-29 15:23:54 -0300

    ajuste dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\FuncionarioExame;
 use App\Models\FuncionarioTreinamento;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use DateTime;
 
 class DashboardController extends Controller
@@ -17,56 +18,44 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $today = new DateTime();
-        dd($today);
 
-        $exames30 = FuncionarioExame::whereDate('data_validade', '<=', $today->addDays(30))
-            ->whereDate('data_validade', '>', $today)
-            ->get()
-            ->map(function ($exame) {
-                $exame->data_validade = Carbon::parse($exame->data_validade)->format('Y-m-d');
-                return $exame;
-            });
+        $hojeSemHora = Carbon::now()->toDateString();
 
-        $exames7 = FuncionarioExame::whereDate('data_validade', '<=', $today->addDays(7))
-            ->whereDate('data_validade', '>', $today)
-            ->get()
-            ->map(function ($exame) {
-                $exame->data_validade = Carbon::parse($exame->data_validade)->format('Y-m-d');
-                return $exame;
-            });
+        $periodo30diasExame = Carbon::now()->addDays(30)->toDateString();
+        $exames30 = FuncionarioExame::whereBetween('data_validade', [$hojeSemHora, $periodo30diasExame])
+            ->whereNotBetween('data_validade', [$hojeSemHora, Carbon::now()->addDays(7)->toDateString()]) // Exclui os exames que faltam 7 dias ou menos
+            ->orderBy('data_validade', 'asc')
+            ->get();
 
-            $examesVencidos = FuncionarioExame::get()
-            ->filter(function ($exame) use ($today) {
-                return Carbon::parse($exame->data_validade)->lessThan($today);
-            })
-            ->map(function ($exame) {
-                $exame->data_validade = Carbon::parse($exame->data_validade)->format('Y-m-d');
-                return $exame;
-            });
-        
-        $treinamentos30 = FuncionarioTreinamento::whereDate('data_validade', '<=', $today->addDays(30))
-            ->whereDate('data_validade', '>', $today)
-            ->get()
-            ->map(function ($treinamento) {
-                $treinamento->data_validade = Carbon::parse($treinamento->data_validade)->format('Y-m-d');
-                return $treinamento;
-            });
+        $periodo7diasExame = Carbon::now()->addDays(7)->toDateString();
+        $exames7 = FuncionarioExame::whereBetween('data_validade', [$hojeSemHora, $periodo7diasExame])
+            ->orderBy('data_validade', 'asc')
+            ->get();
 
-        $treinamentos7 = FuncionarioTreinamento::whereDate('data_validade', '<=', $today->addDays(7))
-            ->whereDate('data_validade', '>', $today)
-            ->get()
-            ->map(function ($treinamento) {
-                $treinamento->data_validade = Carbon::parse($treinamento->data_validade)->format('Y-m-d');
-                return $treinamento;
-            });
+        $examesVencidos = FuncionarioExame::where('data_validade', '<', $hojeSemHora)
+            ->orderBy('data_validade', 'asc')
+            ->get();
 
-        $treinamentosVencidos = FuncionarioTreinamento::whereDate('data_validade', '<', $today)
-            ->get()
-            ->map(function ($treinamento) {
-                $treinamento->data_validade = Carbon::parse($treinamento->data_validade)->format('Y-m-d');
-                return $treinamento;
-            });
+        /*-------------------------------------------------------------------------------------------*/
+        /*-------------------------------------------------------------------------------------------*/
+        /*-------------------------------------------------------------------------------------------*/
+
+        $periodo30diasTreinamento = Carbon::now()->addDays(30)->toDateString();
+        $treinamentos30 = FuncionarioTreinamento::whereBetween('data_validade', [$hojeSemHora, $periodo30diasTreinamento])
+            ->whereNotBetween('data_validade', [$hojeSemHora, Carbon::now()->addDays(7)->toDateString()]) // Exclui os exames que faltam 7 dias ou menos
+            ->orderBy('data_validade', 'asc')
+            ->get();
+
+        $periodo7diasTreinamento = Carbon::now()->addDays(7)->toDateString();
+        $treinamentos7 = FuncionarioTreinamento::whereBetween('data_validade', [$hojeSemHora, $periodo7diasTreinamento])
+            ->orderBy('data_validade', 'asc')
+            ->get();
+
+        $treinamentosVencidos = FuncionarioTreinamento::where('data_validade', '<', $hojeSemHora)
+            ->orderBy('data_validade', 'asc')
+            ->get();
+
+        // dd($hojeSemHora, $examesVencidos);
 
         return view('dashboard.index', compact('exames30', 'exames7', 'examesVencidos', 'treinamentos30', 'treinamentos7', 'treinamentosVencidos'));
     }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,72 +6,102 @@
 
     <body style="background-color: #ffffff">
         <p></p>
-        <br><br><br><br>
+        
 
         <div class="row row-cols-1 row-cols-md-3 g-4 mybackground">
             <div class="col" id="30_dias">
                 <div class="card h-100">
-                    <div class="card-body text-center">
+                    <div class="card-body text-center border border-primary border-2 rounded">
+                        <p>30 Dias ou Menos</p>
                         @foreach ($exames30 as $exame)
-                            <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                            <p>{{ $exame->idFuncionario->nome }}</p>
-                            <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}</p>
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(207, 219, 251) 0%, rgb(176, 176, 219) 100%);">
+                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                    <p>{{ $exame->idFuncionario->nome }}</p>
+                                    <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
+                                    </p>
+                                </div>
+                            </div>
                         @endforeach
 
                         @foreach ($treinamentos30 as $treinamento)
-                            <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                            <p>{{ $treinamento->idFuncionario->nome }}</p>
-                            <p><strong>Data:</strong> {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                            </p>
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(207, 219, 251) 0%, rgb(176, 176, 219) 100%);">
+                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                    <p><strong>Data:</strong>
+                                        {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                    </p>
+                                </div>
+                            </div>
                         @endforeach
-
-                    </div>
-                    <div class="card-footer" style="background-color: #c94203">
-                        <small class="text-muted"> </small>
                     </div>
                 </div>
             </div>
 
             <div class="col" id="7_dias">
                 <div class="card h-100">
-                    <div class="card-body text-center">
+                    <div class="card-body text-center border border-warning border-2 rounded">
+                        <p class="mb-3">7 Dias ou Menos</p>
                         @foreach ($exames7 as $exame)
-                            <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                            <p>{{ $exame->idFuncionario->nome }}</p>
-                            <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}</p>
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(233, 230, 196) 0%, rgb(225, 217, 129) 100%);">
+                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                    <p>{{ $exame->idFuncionario->nome }}</p>
+                                    <p><strong>Data:
+                                        </strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}</p>
+                                </div>
+                            </div>
                         @endforeach
 
                         @foreach ($treinamentos7 as $treinamento)
-                            <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                            <p>{{ $treinamento->idFuncionario->nome }}</p>
-                            <p><strong>Data:</strong> {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                            </p>
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(233, 230, 196) 0%, rgb(225, 217, 129) 100%);">
+                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                    <p><strong>Data:
+                                        </strong>{{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                    </p>
+                                </div>
+                            </div>
                         @endforeach
                     </div>
-                    <div class="card-footer" style="background-color: #1900ff">
-                        <small class="text-muted"> </small>
-                    </div>
+
                 </div>
             </div>
+
             <div class="col" id="vencido">
                 <div class="card h-100">
-                    <div class="card-body text-center">
-                        <p>vencido</p>
+                    <div class="card-body text-center border border-danger border-2 rounded">
+                        <p>Vencidos</p>
                         @foreach ($examesVencidos as $exame)
-                        <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                        <p>{{ $exame->idFuncionario->nome }}</p>
-                        <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}</p>
-                    @endforeach
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(233, 230, 196) 0%, rgb(225, 217, 129) 100%);">
+                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                    <p>{{ $exame->idFuncionario->nome }}</p>
+                                    <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
+                                    </p>
+                                </div>
+                            </div>
+                        @endforeach
 
-                    @foreach ($treinamentosVencidos as $treinamento)
-                        <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                        <p>{{ $treinamento->idFuncionario->nome }}</p>
-                        <p><strong>Data:</strong> {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                        </p>
-                    @endforeach
-                    </div>
-                    <div class="card-footer" style="background-color: #09ff00">
-                        <small class="text-muted"> </small>
+                        @foreach ($treinamentosVencidos as $treinamento)
+                            <div class="card mb-3">
+                                <div class="card-body"
+                                    style="background: linear-gradient(to right, rgb(233, 196, 196) 0%, rgb(225, 129, 129) 100%);">
+                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                    <p><strong>Data:</strong>
+                                        {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                    </p>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
