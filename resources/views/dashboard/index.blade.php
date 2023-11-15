b1ac9abb17900b3610614c1b6ac2ec275b96c1a6
Renan Gabriel <renan.testc@gmail.com>
2023-11-15 15:17:05 -0300

    alteracao no estilo do dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,35 +6,39 @@
 
     <body style="background-color: #ffffff">
         <p></p>
-
-
         <div class="row row-cols-1 row-cols-md-3 g-4 mybackground">
             <div class="col" id="30_dias">
                 <div class="card h-100">
                     <div class="card-body text-center border border-primary border-2 rounded">
                         <p>30 Dias ou Menos</p>
                         @foreach ($exames30 as $exame)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(215, 223, 243) 0%, rgb(197, 197, 237) 100%);">
-                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                                    <p>{{ $exame->idFuncionario->nome }}</p>
-                                    <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
-                                    </p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcExame.edit', ['idFuncionario' => $exame->idFuncionario, 'idExame' => $exame->idExame]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(215, 223, 243) 0%, rgb(197, 197, 237) 100%);">
+                                        <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                        <p>{{ $exame->idFuncionario->nome }}</p>
+                                        <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
 
                         @foreach ($treinamentos30 as $treinamento)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(215, 223, 243) 0%, rgb(197, 197, 237) 100%);">
-                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
-                                    <p><strong>Data:</strong>
-                                        {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                                    </p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcTreinamento.edit', ['idFuncionario' => $treinamento->idFuncionario, 'idTreinamento' => $treinamento->idTreinamento]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(215, 223, 243) 0%, rgb(197, 197, 237) 100%);">
+                                        <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                        <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                        <p><strong>Data:</strong>
+                                            {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
                     </div>
@@ -46,27 +50,34 @@
                     <div class="card-body text-center border border-warning border-2 rounded">
                         <p class="mb-3">7 Dias ou Menos</p>
                         @foreach ($exames7 as $exame)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(241, 239, 215) 0%, rgb(240, 234, 174) 100%);">
-                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                                    <p>{{ $exame->idFuncionario->nome }}</p>
-                                    <p><strong>Data:
-                                        </strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}</p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcExame.edit', ['idFuncionario' => $exame->idFuncionario, 'idExame' => $exame->idExame]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(241, 239, 215) 0%, rgb(240, 234, 174) 100%);">
+                                        <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                        <p>{{ $exame->idFuncionario->nome }}</p>
+                                        <p><strong>Data:
+                                            </strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
 
                         @foreach ($treinamentos7 as $treinamento)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(241, 239, 215) 0%, rgb(240, 234, 174) 100%);">
-                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
-                                    <p><strong>Data:
-                                        </strong>{{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                                    </p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcTreinamento.edit', ['idFuncionario' => $treinamento->idFuncionario, 'idTreinamento' => $treinamento->idTreinamento]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(241, 239, 215) 0%, rgb(240, 234, 174) 100%);">
+                                        <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                        <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                        <p><strong>Data:
+                                            </strong>{{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
                     </div>
@@ -79,27 +90,33 @@
                     <div class="card-body text-center border border-danger border-2 rounded">
                         <p>Vencidos</p>
                         @foreach ($examesVencidos as $exame)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(240, 210, 210) 0%, rgb(228, 162, 162) 100%);">
-                                    <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
-                                    <p>{{ $exame->idFuncionario->nome }}</p>
-                                    <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
-                                    </p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcExame.edit', ['idFuncionario' => $exame->idFuncionario, 'idExame' => $exame->idExame]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(240, 210, 210) 0%, rgb(228, 162, 162) 100%);">
+                                        <p><strong>Exame:</strong> {{ $exame->idExame->exame }}</p>
+                                        <p>{{ $exame->idFuncionario->nome }}</p>
+                                        <p><strong>Data:</strong>{{ \Carbon\Carbon::parse($exame->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
 
                         @foreach ($treinamentosVencidos as $treinamento)
-                            <div class="card mb-3">
-                                <div class="card-body"
-                                    style="background: linear-gradient(to right, rgb(240, 210, 210) 0%, rgb(228, 162, 162) 100%);">
-                                    <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
-                                    <p>{{ $treinamento->idFuncionario->nome }}</p>
-                                    <p><strong>Data:</strong>
-                                        {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
-                                    </p>
-                                </div>
+                            <div class="card mb-3 highlight-card">
+                                <a href="{{ route('funcTreinamento.edit', ['idFuncionario' => $treinamento->idFuncionario, 'idTreinamento' => $treinamento->idTreinamento]) }}"
+                                    class="card-link">
+                                    <div class="card-body"
+                                        style="background: linear-gradient(to right, rgb(240, 210, 210) 0%, rgb(228, 162, 162) 100%);">
+                                        <p><strong>Treinamento:</strong> {{ $treinamento->idTreinamento->treinamento }}</p>
+                                        <p>{{ $treinamento->idFuncionario->nome }}</p>
+                                        <p><strong>Data:</strong>
+                                            {{ \Carbon\Carbon::parse($treinamento->data_validade)->format('d/m/Y') }}
+                                        </p>
+                                    </div>
+                                </a>
                             </div>
                         @endforeach
                     </div>
